2a7c42960e939bce3b0b15298fb98caa01b830ea
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-01-17 14:55:56 +0530

    Create a user using phone number

--- one-time-password/functions/create_user.js
+++ one-time-password/functions/create_user.js
@@ -1,3 +1,20 @@
+const admin = require('firebase-admin');
+
 module.exports = function(req, res) {
-  res.send(req.body);
+  // Verify the user provided a phone
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' });
+  }
+
+  // Format the phone number to remove dashes and parens
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+
+  // Create a new user account using that phone number
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }));
+
+  // Respond to the user request, saying the account was made
+  
+
 }
