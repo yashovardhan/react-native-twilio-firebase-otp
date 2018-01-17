cc4e333bbcf7cf5b982021de2f24f6b6d269f1fb
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-01-17 14:18:59 +0530

    Add serviceAccount to interact with Database

--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -1,4 +1,11 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
+const serviceAccount = require('./service-account.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://one-time-password-d3524.firebaseio.com"
+});
 
 exports.createUser = functions.https.onRequest(createUser);
