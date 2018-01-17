ffbde29b82a3c5e1395684a28eecc84ffb5420df
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-01-17 13:52:08 +0530

    clearing up the index.js file

--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -1,11 +1,4 @@
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 exports.createUser = functions.https.onRequest(createUser);
