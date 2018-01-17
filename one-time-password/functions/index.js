c7435a4d8f63211da44be079dd9b106272938a37
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-01-17 13:21:39 +0530

    make new firebase function createUser

--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const createUser = require('./create_user');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +7,5 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.createUser = functions.https.onRequest(createUser);
