a8ce75fae94f9e564403a6ed1e05ec484144f3a9
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-01-31 23:20:26 +0530

    App.js: Integrate SignUpForm.js into app

--- firebase-auth/App.js
+++ firebase-auth/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SignUpForm from './components/SignUpForm';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <SignUpForm />
       </View>
     );
   }
