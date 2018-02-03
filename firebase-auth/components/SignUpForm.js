e61c401e67bd1eb96f7fccac3d85411c2bd6db5b
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-02-03 19:11:53 +0530

    SignUpForm.js: Invoke firebase functions

--- firebase-auth/components/SignUpForm.js
+++ firebase-auth/components/SignUpForm.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
+import axios from 'axios';
+
+const ROOT_URL = 'https://us-central1-one-time-password-d3524.cloudfunctions.net'
 
 class SignUpForm extends Component {
   state = { phone: '' };
 
-  handleSubmit = () => {
-
+  handleSubmit = async () => {
+    try {
+      await axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone })
+      await axios.post(`${ROOT_URL}/requestOTP`, { phone: this.state.phone })
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   render() {
