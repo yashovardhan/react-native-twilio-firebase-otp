3d4cdc7cc4c480d8aea7457c6845983408a7d465
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-02-03 20:05:36 +0530

    integrate SignInForm into App.js

--- firebase-auth/App.js
+++ firebase-auth/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SignUpForm from './components/SignUpForm';
+import SignInForm from './components/SignInForm';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <SignUpForm />
+        <SignInForm />
       </View>
     );
   }
@@ -17,6 +19,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
